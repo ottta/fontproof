19175ac2e751f041e8e3717871f5f674375681de
Taufik Oktama <ottta@unforma.club>
2023-03-01 09:20:15 +0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Fontproof by Unforma Club</title>
+                <meta name="description" content="Font proofing tools" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            Page Index
+        </>
+    );
+}
