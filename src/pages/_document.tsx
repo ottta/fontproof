c124448d000f6f3e3576f5a0bda40699f8cc7305
Taufik Oktama <ottta@unforma.club>
2023-05-19 13:46:58 +0700

    feat: Add new font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <title>Fontproof by Unforma Club</title>
+                <link
+                    rel="preload"
+                    type="font/ttf"
+                    as="font"
+                    href="https://font.unforma.club/files/Olig-Bold.ttf"
+                    crossOrigin="anonymous"
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
